33af8a211c4c353503bb55e84a2c43f64e7b14a4
Yesha016 <yum2@njit.edu>
2019-10-30 17:48:35 -0400

    Fix: Code fixing in CalculatorTests file

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -3,44 +3,38 @@ from Calculator import Calculator
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
 
     def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+        self.assertIsInstance(self.calculator, Calculator)
 
     def test_results_property_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.result, 0)
+        self.assertEqual(self.calculator.result, 0)
 
     def test_add_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.add(2, 2), 4)
-        self.assertEqual(calculator.result, 4)
+        self.assertEqual(self.calculator.add(2, 2), 4)
+        self.assertEqual(self.calculator.result, 4)
 
     def test_subtract_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.subtract(2, 2), 0)
-        self.assertEqual(calculator.result, 0)
+        self.assertEqual(self.calculator.subtract(2, 2), 0)
+        self.assertEqual(self.calculator.result, 0)
 
     def test_multiply_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.multiply(2, 2), 4)
-        self.assertEqual(calculator.result, 4)
+        self.assertEqual(self.calculator.multiply(2, 2), 4)
+        self.assertEqual(self.calculator.result, 4)
 
     def test_divide_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.divide(8, 4), 2)
-        self.assertEqual(calculator.result, 2)
+        self.assertEqual(self.calculator.divide(8, 4), 2)
+        self.assertEqual(self.calculator.result, 2)
 
     def test_square_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.square(4), 16)
-        self.assertEqual(calculator.result, 16)
+        self.assertEqual(self.calculator.square(4), 16)
+        self.assertEqual(self.calculator.result, 16)
 
     def test_sqrt_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.sqrt(4), 2)
-        self.assertEqual(calculator.result, 2)
+        self.assertEqual(self.calculator.sqrt(4), 2)
+        self.assertEqual(self.calculator.result, 2)
 
 if __name__ == '__main__':
     unittest.main()
